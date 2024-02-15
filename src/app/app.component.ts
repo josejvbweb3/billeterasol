357f953ad34039f0f7db4e3cb5aa5f88f9a773cc
Jose JVB <bolufer@gmail.com>
2024-02-15 17:29:25 +0100

    chore: eliminar archivos innecesarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'billeterasol-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: 'Wallet',
 })
 export class AppComponent {
-  title = 'billeterasol';
+  
 }
