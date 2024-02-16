b8671a6560afae902a6ca6255dee8a50b646401d
Jose JVB <bolufer@gmail.com>
2024-02-16 09:02:09 +0100

    feat: integrar el wallet-adapter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { HdWalletMultiButtonComponent} from '@heavy-duty/wallet-adapter-material';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'billeterasol-root',
-  template: 'Wallet',
+  template: `
+    <header class="py-8">
+      <h1 class="text-5xl text-center mb-4">Wallet</h1>
+
+      <div class="flex justify-center">
+      <hd-wallet-multi-button></hd-wallet-multi-button>
+      </div>
+      
+    </header>
+  
+  `,
 })
 export class AppComponent {
   
