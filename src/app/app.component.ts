8fd85bb33b71d10c63d21e0d9e87b0b7e0771af8
Jose JVB <bolufer@gmail.com>
2024-02-16 10:33:03 +0100

    feat: mostrar balance de usdc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HdWalletMultiButtonComponent} from '@heavy-duty/wallet-adapter-material';
+import { ShyftApiService } from './shyft-api.services';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { computedAsync } from 'ngxtension/computed-async';
 
 @Component({
   standalone: true,
@@ -13,11 +17,25 @@ import { HdWalletMultiButtonComponent} from '@heavy-duty/wallet-adapter-material
       <div class="flex justify-center">
       <hd-wallet-multi-button></hd-wallet-multi-button>
       </div>
-      
+
+      @if (account()) {
+        <div 
+          class="absolute top-4 left-4 flex justify-center items-center gap-2">
+          <img [src]="account()?.info?.image" class="w-8 h-8" />
+          <p class="text-xl">{{ account()?.balance }}</p>
+        </div>
+      }
+
     </header>
-  
   `,
 })
 export class AppComponent {
-  
+  private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+
+  readonly account = computedAsync(
+    () => this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
+    { requireSync: true },
+  );
 }
